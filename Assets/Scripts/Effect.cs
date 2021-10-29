6ec91607d4bc728dfc6faab298fd30bea8ac541d
Maksim Tretyakov <m.tretyakov@kefirgames.com>
2021-10-29 21:04:11 +0300

    add: effect

--- Assets/Scripts/Effect.cs
+++ Assets/Scripts/Effect.cs
@@ -3,9 +3,15 @@ using UnityEngine;
 public class Effect : MonoBehaviour
 {
     [SerializeField] private ParticleSystem flash;
+    [SerializeField] private ParticleSystem hit;
 
     public void FlashFxPlay()
     {
         flash.Play();
+    }
+
+    public void HitFxPlay()
+    {
+        hit.Play();
     }
 }
