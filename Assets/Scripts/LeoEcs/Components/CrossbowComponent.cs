9199674956bb956790643aac0d8feebaf3f55db4
Artem Kupriyanov <a.kupriyanov@kefirgames.com>
2021-10-29 18:29:27 +0300

    upd: add change bullets

--- Assets/Scripts/LeoEcs/Components/CrossbowComponent.cs
+++ Assets/Scripts/LeoEcs/Components/CrossbowComponent.cs
@@ -1,3 +1,4 @@
+using System;
 using Leopotam.Ecs;
 using Sirenix.OdinInspector;
 using UnityEngine;
@@ -7,15 +8,26 @@ namespace Client
     struct CrossbowComponent : IComponent
     {
         public Transform point;
-        public GameObject Bullet;
+        public BulletData[] data;
         public Animator animation;
+
+        [HideInInspector]
+        public BulletType currentType;
 #if UNITY_EDITOR
         [ShowInInspector]
         public string Doc => "Компонент который отвечает за хранение пули";
 #endif
         public void SetOwner(ref EcsEntity entity, out IComponent component)
         {
+            currentType = BulletType.Red;
             component = entity.Get<CrossbowComponent>() = this;
         }
+    }
+
+    [Serializable]
+    public struct BulletData
+    {
+        public GameObject Bullet;
+        public BulletType type;
     }
 }
